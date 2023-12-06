9b86a1ede872cf7ceb53359174f78acb5ebe9d20
陳中和 <zhong31415926@gmail.com>
2023-12-06 21:24:48 +0800

    123

--- lab8/lab8.v
+++ lab8/lab8.v
@@ -23,6 +23,7 @@ module Lab8(
     // We have connected the motor and sonic_top modules in the template file for you.
     //test
     //i am happy
+    //are you sure ?
     // TODO: control the motors with the information you get from ultrasonic sensor and 3-way track sensor.
     
     sonic_top B(
