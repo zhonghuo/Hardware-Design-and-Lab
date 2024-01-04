d0b11dd008356ab94338b49b006b371450d1d3a9
陳中和 <zhong31415926@gmail.com>
2024-01-05 00:50:15 +0800

    好奇怪喔

--- final_project/map1.v
+++ final_project/map1.v
@@ -8,21 +8,21 @@ module map1(
     input wire [9:0] vga_v,  
     inout PS2_DATA, 
     inout PS2_CLK, 
-    output wire [16:0] addr, 
+    output reg [16:0] addr, 
     output reg clear
 );
     wire collision_with_player1, collision_with_player2;
     wire en_ceiling, en_floor;
+    wire [16:0] addr_ceiling, addr_floor;
+
     terrain map1_ceiling(
         .clk(clk),
         .rst(rst),
         .terrain_props({10'd0, 10'd0, 10'd320, 10'd10}),
         .en(en_ceiling),
         .addr(addr_ceiling),
-        .disp_h(0),
-        .dir_disp_h(0),
-        .disp_v(10'd220),
-        .dir_disp_v(1),
+        .mem_pivot_h(0),
+        .mem_pivot_v(10'd220),
         .vga_h(vga_h),
         .vga_v(vga_v),
         .collision_with_player1(collision_with_player1),
@@ -35,24 +35,18 @@ module map1(
         .terrain_props({10'd0, 10'd230, 10'd320, 10'd10}),
         .en(en_floor),
         .addr(addr_floor),
-        .disp_h(0),
-        .dir_disp_h(0),
-        .disp_v(10'd10),
-        .dir_disp_v(0),
+        .mem_pivot_h(10'd1),
+        .mem_pivot_v(10'd220),
         .vga_h(vga_h),
         .vga_v(vga_v),
         .collision_with_player1(collision_with_player1),
         .collision_with_player2(collision_with_player2)
     );
 
-    assign addr = (en_ceiling) ? (
-        addr_ceiling
-    ) : (
-        (en_floor) ? (
-            addr_floor
-        ) : (
-            12900
-        )
-    );
+    always @* begin
+        if(en_ceiling) addr = addr_ceiling;
+        else if(en_floor) addr = addr_floor;
+        else addr = 12900;
+    end
 
 endmodule
--- final_project/obj_terrain.v
+++ final_project/obj_terrain.v
@@ -6,14 +6,14 @@ module terrain(
     output reg collision_with_player1,
     output reg collision_with_player2, 
     output wire [16:0] addr, 
-    output wire en,
+    output reg en,
     input wire [39:0] terrain_props, 
     //input wire [39:0] player1_props, 
     //input wire [39:0] player2_props, 
-    input wire [9:0] disp_h, disp_v,
-    input wire dir_disp_h, dir_disp_v,
     input wire [9:0] vga_h,
     input wire [9:0] vga_v,
+    input wire [9:0] mem_pivot_h,
+    input wire [9:0] mem_pivot_v,
     input clk,
     input rst
 );
@@ -30,24 +30,22 @@ module terrain(
     wire [9:0] player2_width = player2_props[19:10];
     wire [9:0] player2_height = player2_props[9:0];*/
 
-    assign en = ((vga_h >> 1) >= pivot_h && (vga_h >> 1) <= (pivot_h + width) && (vga_v >> 1) >= pivot_v && (vga_v >> 1) <= (pivot_v + height));
+    wire [9:0] h, v;
+    assign h = vga_h >> 1;
+    assign v = vga_v >> 1;
 
-    assign addr = ((vga_h >> 1) >= pivot_h && (vga_h >> 1) <= (pivot_h + width) && (vga_v >> 1) >= pivot_v && (vga_v >> 1) <= (pivot_v + height)) ? (
-        (dir_disp_h == 1 && dir_disp_v == 1) ? (
-            ((vga_h >> 1) + disp_h) + ((vga_v >> 1) + disp_v) * 320
-        ) : (
-            (dir_disp_h == 1 && dir_disp_v == 0) ? (
-                ((vga_h >> 1) + disp_h) + ((vga_v >> 1) - disp_v) * 320
-            ) : (
-                (dir_disp_h == 0 && dir_disp_v == 1) ? (
-                    ((vga_h >> 1) - disp_h) + ((vga_v >> 1) + disp_v) * 320
-                ) : (
-                    ((vga_h >> 1) - disp_h) + ((vga_v >> 1) - disp_v) * 320
-                )   
-            )
-        )
-    ) : (
-        12900
-    );
+    reg [9:0] disp_h, disp_v;
+    always @* begin
+        if(v-pivot_v>=0 && v-pivot_v<height && h-pivot_h>=0 && h-pivot_h<width) begin
+            en = 1;
+            disp_h = h-pivot_h;
+            disp_v = v-pivot_v;
+        end else begin
+            en = 0;
+            disp_h = 0;
+            disp_v = 0;
+        end
+    end
+    assign addr = disp_h + mem_pivot_h + 320*(disp_v + mem_pivot_v);
 
 endmodule
