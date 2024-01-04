659a51ac8ae812e6d231758bdd5ebe09d8d45919
陳中和 <zhong31415926@gmail.com>
2024-01-04 22:25:16 +0800

    terrain 開兩個的情況，會multiple driver

--- final_project/map1.v
+++ final_project/map1.v
@@ -11,7 +11,7 @@ module map1(
     output wire [16:0] addr, 
     output reg clear
 );
-    reg collision_with_player1, collision_with_player2;
+    wire collision_with_player1, collision_with_player2;
     
     terrain map1_ceiling(
         .clk(clk),
@@ -20,8 +20,23 @@ module map1(
         .addr(addr),
         .disp_h(0),
         .dir_disp_h(0),
-        .disp_v(10'd320),
+        .disp_v(10'd220),
         .dir_disp_v(1),
+        .vga_h(vga_h),
+        .vga_v(vga_v),
+        .collision_with_player1(collision_with_player1),
+        .collision_with_player2(collision_with_player2)
+    );
+
+    terrain map1_floor(
+        .clk(clk),
+        .rst(rst),
+        .terrain_props({10'd0, 10'd230, 10'd320, 10'd10}),
+        .addr(addr),
+        .disp_h(0),
+        .dir_disp_h(0),
+        .disp_v(10'd10),
+        .dir_disp_v(0),
         .vga_h(vga_h),
         .vga_v(vga_v),
         .collision_with_player1(collision_with_player1),
