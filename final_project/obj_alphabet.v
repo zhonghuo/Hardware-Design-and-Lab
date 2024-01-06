b37694c64551c5c1b165c68cfec3b92f518ed13e
hyouka0904 <charles.kp0904@gmail.com>
2024-01-06 15:43:48 +0800

    update cover but seems like there are bugs

---
+++ final_project/obj_alphabet.v
@@ -0,0 +1,34 @@
+module abc(
+    //addr is the index of mem
+    //en indicate that screen should output this object
+    output wire [15:0] addr, 
+    output reg en,
+    input wire [9:0] vga_h,
+    input wire [9:0] vga_v,
+    input wire [9:0] mem_pivot_h,
+    input wire [9:0] mem_pivot_v,
+    input wire [9:0] pivot_h,
+    input wire [9:0] pivot_v,
+    input wire [9:0] width,
+    input wire [9:0] height
+);
+
+    wire [9:0] h, v;
+    assign h = vga_h >> 2;
+    assign v = vga_v >> 2;
+
+    reg [9:0] disp_h, disp_v;
+    always @* begin
+        if(v-pivot_v>=0 && v-pivot_v<height && h-pivot_h>=0 && h-pivot_h<width) begin
+            en = 1;
+            disp_h = h-pivot_h;
+            disp_v = v-pivot_v;
+        end else begin
+            en = 0;
+            disp_h = 0;
+            disp_v = 0;
+        end
+    end
+    assign addr = disp_h + mem_pivot_h + 160*(disp_v + mem_pivot_v);
+
+endmodule
